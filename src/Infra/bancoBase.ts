46512d43d6ee93db44eccfc44661bb79287f9065
RapidLupine <rapidlupine@gmail.com>
2022-05-09 01:54:42 -0300

    build: :construction_worker: Movido a localização das variaveis de ambientes

--- src/Infra/bancoBase.ts
+++ src/Infra/bancoBase.ts
@@ -1,6 +1,18 @@
 import { ServiceAccount } from 'firebase-admin';
 import admin = require('firebase-admin');
-import serviceAccount = require('../../enviroment/firebaseKey.json');
+const serviceAccount = {
+  type: 'service_account',
+  project_id: 'informacoeslancamentosspacex',
+  private_key_id: process.env.KEY_ID,
+  private_key: process.env.KEY_SECRET,
+  client_email: 'informacoeslancamentosspacex@appspot.gserviceaccount.com',
+  client_id: '117838830393450967254',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/informacoeslancamentosspacex%40appspot.gserviceaccount.com',
+};
 
 export abstract class BancoBase {
   private _app: admin.app.App;
@@ -14,7 +26,7 @@ export abstract class BancoBase {
     });
 
     this.database = this._app.database(
-      'https://informacoeslancamentosspacex-default-rtdb.firebaseio.com/',
+      'https://informacoeslancamentosspacex-default-rtdb.firebaseio.com/'
     );
   }
 }
